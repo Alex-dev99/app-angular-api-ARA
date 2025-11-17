bf4446fbc43f526acc6818020f925e1315b0f0f0
Alejandro <rodriguez.arale24@triana.salesianos.edu>
2025-11-17 14:14:19 +0100

    "hecho"

--- src/app/Vehicles.routes.ts
+++ src/app/Vehicles.routes.ts
@@ -1,12 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { Routes } from '@angular/router';
-import { VehiclesListPage } from './vehicles-list-page';
+import { VehiclesDetailPage } from './vehicles-detail-page/vehicles-detail-page';
+import { VehiclesListPage } from './vehicles-list-page/vehicles-list-page';
 
 
 export const routes: Routes = [
   {
     path: 'list',
     component: VehiclesListPage,
+  },
+    {
+    path: 'detail',
+    component: VehiclesDetailPage,
   },
 ];
